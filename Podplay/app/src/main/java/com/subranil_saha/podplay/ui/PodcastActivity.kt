e14a314189c1d410caa9031776570a81fe74a585
ShuvranilSaha <subranil.saha@gmail.com>
2021-07-20 13:36:29 +0530

    Issue #PP feat: added custom search activity

--- Podplay/app/src/main/java/com/subranil_saha/podplay/ui/PodcastActivity.kt
+++ Podplay/app/src/main/java/com/subranil_saha/podplay/ui/PodcastActivity.kt
@@ -1,7 +1,12 @@
 package com.subranil_saha.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import com.subranil_saha.podplay.R
 import com.subranil_saha.podplay.repository.ItunesRepo
@@ -14,13 +19,37 @@ class PodcastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
+    }
 
+    private fun performSearch(term: String) {
         val itunesService = ItunesService.instance
         val itunesRepo = ItunesRepo(itunesService)
-
         GlobalScope.launch {
-            val results = itunesRepo.searchByTerm("Android Developer")
+            val results = itunesRepo.searchByTerm(term)
             Log.i(TAG, "Results = ${results.body()}")
         }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (intent.action === Intent.ACTION_SEARCH) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?: return
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+        val searchMenuItem = menu?.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        return true
     }
 }
